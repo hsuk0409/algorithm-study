a34a22e2e24d34c52e404325f6766f1d054174a3
earstone <hsuk0409>
2024-02-15 11:29:15 +0900

    중복 있는 순열 추가

--- java-algorithm/src/test/java/algorithm/study/PermutationTest.java
+++ java-algorithm/src/test/java/algorithm/study/PermutationTest.java
@@ -30,6 +30,27 @@ public class PermutationTest {
         }
     }
 
+    @Test
+    @DisplayName("중복있는 순열")
+    public void permutationTest() {
+        int[] arr = {3, 6, 9, 12};
+        int[] output = new int[arr.length];
+
+        permutation(arr, output, 0, arr.length, 3);
+    }
+
+    static void permutation(int[] arr, int[] output, int depth, int n, int r) {
+        if (depth == r) {
+            myPrint(output, r);
+            return;
+        }
+
+        for (int i = 0; i < n; ++i) {
+            output[depth] = arr[i];
+            permutation(arr, output, depth + 1, n, r);
+        }
+    }
+
     static void myPrint(int[] output, int r) {
         for (int i = 0; i < r; ++i) {
             System.out.print(output[i] + " ");
