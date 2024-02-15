6fa0e9bc7332cd539470384068392f7c3bc8b4fb
earstone <hsuk0409>
2024-02-15 09:13:47 +0900

    버블정렬 클래스명 수정

---
+++ java-algorithm/src/test/java/algorithm/study/sort/BubbleSortTest.java
@@ -0,0 +1,28 @@
+package algorithm.study.sort;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class BubbleSortTest {
+
+    @Test
+    @DisplayName("버블 정렬을 구현한다.")
+    public void bubbleSort() {
+        int[] numArr = {3, 5, 2, 9, 7, 4, 3, 1, 10};
+
+        for (int i = 0; i < numArr.length - 1; ++i) {
+            for (int k = 0; k < numArr.length - i - 1; ++k) {
+                if (numArr[k] > numArr[k + 1]) {
+                    int tmp = numArr[k];
+                    numArr[k] = numArr[k + 1];
+                    numArr[k + 1] = tmp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(numArr));
+    }
+
+}
